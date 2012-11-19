af589252991da8d578d2318fa8942ab6476685a7
retracell <retracell@gmail.com>
2012-11-19 02:53:37 -0500

    fixing lookup, replaced colors using colors resource, pressing enter key performs action

--- src/com/learn/resistor/Resistor.java
+++ src/com/learn/resistor/Resistor.java
@@ -1,18 +1,15 @@
 package com.learn.resistor;
 
 public class Resistor {
-	private int resistance;
+	private String resistance;
 	private int tolerance;
-	public int band[] = new int[4];
+	public String band[] = new String[4];
+	String[] colourCode = { "black", "brown", "red", "orange", "yellow",
+			"green", "blue", "violet", "gray", "white", "gold", "silver", "none" };
 
-	String[] colourCode = { "Black", "Brown", "Red", "Orange", "Yellow",
-			"Green", "Blue", "Violet", "Gray", "White", "Gold", "Silver", "None" };
-	int[] bandColours = new int[4];
-
-	public Resistor(int resistance, int tolerance) {
+	public Resistor(String resistance, int tolerance) {
 		this.resistance = resistance;
 		this.tolerance = tolerance;
-		determineBandColours();
 	}
 
 	// This is the general method that is used to determine what colour the
@@ -20,45 +17,27 @@ public class Resistor {
 	// We can return an array index instead of colour string as long as all 
 	// colours are in set order.
 	private void determineBandColours() {
-		convertResistanceToBand();
-		bandColours[0] = band[0];
-		bandColours[1] = band[1];
-		// This if statement is necessary to check if the 2nd digit is 0.
-		// If it is, then we subtract by one index because it is one less
-		// multiplier.
-		if (Integer.toString(resistance).toCharArray()[1] == '0') {
-			bandColours[2] = band[2] - 1;
-		} else {
-			bandColours[2] = band[2];
-		}
-		switch (band[3]) {
+		band[0] = colourCode[Integer.parseInt(resistance.substring(0, 1))];
+		band[1] = colourCode[Integer.parseInt(resistance.substring(1, 2))];
+		band[2] = colourCode[determineCountMultiplier(resistance)];
+		switch (tolerance) {
 		case 5:
-			bandColours[3] = 10; //colourCode index 10
+			band[3] = colourCode[10];
 			break;
 		case 10:
-			bandColours[3] = 11; //index 11
+			band[3] = colourCode[11];
 			break;
 		case 20:
-			bandColours[3] = 12; //index 12
+			band[3] = colourCode[12];
 		}
 	}
-
-	// This method parses the integer into a string and determines from an array
-	// what each band's colour is.
-	private void convertResistanceToBand() {
-		String resistanceAsString = Integer.toString(resistance);
-		band[0] = Integer.parseInt(resistanceAsString.substring(0, 1));
-		band[1] = Integer.parseInt(resistanceAsString.substring(1, 2));
-		band[2] = determineCountMultiplier(resistanceAsString);
-		band[3] = tolerance;
-	}
-
+	
 	// Used for the third band by counting the number of 0s to determine what
 	// multiplier is applied to the resistance.
 	private int determineCountMultiplier(String resistance) {
 		char[] resistanceAsCharArray = resistance.toCharArray();
 		int count = 0;
-		for (int i = 0; i < resistanceAsCharArray.length; ++i) {
+		for (int i = 2; i < resistanceAsCharArray.length; ++i) {
 			if (resistanceAsCharArray[i] == '0') {
 				count++;
 			}
@@ -67,16 +46,8 @@ public class Resistor {
 	}
 
 	// Called by the activity, returns the band colours as an array.
-	public int[] getBandColours() {
-		return bandColours;
-	}
-
-	// Testing methods...
-	public String arrayToString(int[] array) {
-		String tempString = "";
-		for (int i = 0; i < array.length; ++i) {
-			tempString = tempString + array[i] + " ";
-		}
-		return tempString;
+	public String[] getBandColours() {
+		determineBandColours();
+		return band;
 	}
 }
--- src/com/learn/resistor/ResistorLookup.java
+++ src/com/learn/resistor/ResistorLookup.java
@@ -1,12 +1,18 @@
 package com.learn.resistor;
 
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.TextureView;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ResistorLookup extends Activity {
@@ -16,78 +22,70 @@ public class ResistorLookup extends Activity {
 	AlertDialog errorDialog;
 	View colourbands[] = new View[4];
 	int hex = 0;
-
-	// Should change this into res/values in the future.
-	int[] hexColours = { 0xFF000000, 0xFF964B00, 0xFFFF0000, 0xFFFF7F00, 0xFFFFFF00,
-			0xFF00FF00, 0xFF0000FF, 0xFF7F00FF, 0xFF808080, 0xFFFFFFFF, 0xFFFFD700,
-			0xFFC9C0BB };
+	
+	HashMap<String, Integer> map = new HashMap<String, Integer>() {
+		{
+			put("black", 0xFF000000);
+			put("brown", 0xFF964B00);
+			put("red", 0xFFFF0000);
+			put("orange", 0xFFFF7F00);
+			put("yellow", 0xFFFFFF00);
+			put("green", 0xFF00FF00);
+			put("blue", 0xFF0000FF);
+			put("violet", 0xFF7F00FF);
+			put("gray", 0xFF808080);
+			put("white", 0xFFFFFFFF);
+			put("gold", 0xFFFFD700);
+			put("silver", 0xFFC9C0BB);
+			put("none", 0x00000000);
+		}
+	};
 
 	public void onCreate(Bundle me) {
 		super.onCreate(me);
 		setContentView(R.layout.main);
-		// The hexColours are listed in this order:
-		// BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GRAY, WHITE
-		// GOLD, SILVER
+		
+		colourbands[0] = (View) findViewById(R.id.band1);
+		colourbands[1] = (View) findViewById(R.id.band2);
+		colourbands[2] = (View) findViewById(R.id.band3);
+		colourbands[3] = (View) findViewById(R.id.band4);
 
-		colourbands[0] = (View) findViewById(R.id.colour1);
-		colourbands[1] = (View) findViewById(R.id.colour2);
-		colourbands[2] = (View) findViewById(R.id.colour3);
-		colourbands[3] = (View) findViewById(R.id.colour4);
-
-		errorDialog = new AlertDialog.Builder(this).create();
-		errorDialog.setTitle("Error");
-		errorDialog.setButton("OK", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				errorDialog.dismiss();
-			}
-		});
 		resistanceNumber = (EditText) findViewById(R.id.resistance_value);
+		resistanceNumber.setOnEditorActionListener(listener);
 		myToleranceGroup = (RadioGroup) findViewById(R.id.tolerances);
 	}
+	
+	TextView.OnEditorActionListener listener = new TextView.OnEditorActionListener() {
+		
+		public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+			if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_DOWN) {
+				getResistanceClicked(v);
+			}
+			return true;
+		}
+	};
 
 	public void getResistanceClicked(View v) {
-		int resistance = 0;
+		String resistance;
 		int tolerance = 0;
-		String resistanceString;
 
-		resistanceString = resistanceNumber.getText().toString();
-		if (myToleranceGroup.getCheckedRadioButtonId() == R.id.five_percent) {
-			tolerance = 5;
-		} else if (myToleranceGroup.getCheckedRadioButtonId() == R.id.ten_percent) {
-			tolerance = 10;
+		resistance = resistanceNumber.getText().toString();
+		if (myToleranceGroup.getCheckedRadioButtonId() == R.id.five_percent) tolerance = 5;
+		else if (myToleranceGroup.getCheckedRadioButtonId() == R.id.ten_percent) tolerance = 10;
+		else tolerance = 20;
+
+		if (resistance.length() > 1) {
+			updateResistorImage(resistance, tolerance);
+		} else if (resistance.length() == 1) {
+			errorDialog.setMessage("Single digit resistance is not yet supported Please try again.");
+			errorDialog.show();
 		} else {
-			tolerance = 20;
-		}
-
-		if (Integer.parseInt(resistanceString) != 0) {
-			if (resistanceString.length() > 1) {
-				resistance = Integer.parseInt(resistanceString);
-				myResistor = new Resistor(resistance, tolerance);
-				// Toast toast = Toast.makeText(getApplicationContext(),
-				// myResistor.arrayToString(myResistor.band),
-				// Toast.LENGTH_LONG);
-				int[] tempArray = myResistor.getBandColours();
-
-				Toast toast = Toast.makeText(getApplicationContext(),
-						arrayToString(tempArray), Toast.LENGTH_LONG);
-				toast.show();
-
-				for (int i = 0; i < colourbands.length; i++) {
-					colourbands[i].setBackgroundColor(hexColours[tempArray[i]]);
-				}
-			} else if (resistanceString.length() == 1) {
-				errorDialog
-						.setMessage("Single digit resistance is not yet supported Please try again.");
-				errorDialog.show();
-			}
-		} else {
-			errorDialog
-					.setMessage("You entered an invalid resistance value. Please try again.");
+			errorDialog.setMessage("You entered an invalid resistance value. Please try again.");
 			errorDialog.show();
 		}
 	}
 
-	public String arrayToString(int[] array) {
+	public String arrayToString(String[] array) {
 		String tempString = "";
 		for (int i = 0; i < array.length; ++i) {
 			tempString = tempString + array[i] + " ";
@@ -95,7 +93,16 @@ public class ResistorLookup extends Activity {
 		return tempString;
 	}
 
-	public void updateResistorImage(String[] resistorColours) {
-
+	public void updateResistorImage(String resistance, int tolerance) {
+		myResistor = new Resistor(resistance, tolerance);
+		String[] bands = myResistor.getBandColours();
+		
+		Toast toast = Toast.makeText(getApplicationContext(),
+				arrayToString(bands), Toast.LENGTH_LONG);
+		toast.show();
+		
+		for (int i = 0; i < colourbands.length; i++) {
+			colourbands[i].setBackgroundColor(map.get(bands[i]));
+		}
 	}
 }
